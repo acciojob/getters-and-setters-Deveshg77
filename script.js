8b406c5b6440aaf547fc941dba96240940b741f4
Devesh Sudhir Gunde <deveshgunde77@gmail.com>
2024-07-22 17:12:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age) {
+        this._name = name;
+        this._age = age;
 
-class Student extends Person {}
+		   get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    // Setter for age
+    set age(age) {
+        this._age = age;
+    }
+}
+}
 
+class Student extends Person {
+	constructor(name, age) {
+        super(name, age);
+
+		study() {
+        console.log(`${this.name} is studying.`);
+    }
+}
+
+class Teacher extends Person {
+	constructor(name, age) {
+        super(name, age);
+}
+let student1 = new Student('Devesh', 20);
+let teacher1 = new Teacher('Mr. Jayesh', 35);
+
+student1.study(); 
+teacher1.teach(); 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
